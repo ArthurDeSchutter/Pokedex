b20d7ae987dfb797f5a47e27ec5aed15f78f9424
ArthurDeSchutter <deschutterarthur1@gmail.com>
2021-03-21 12:12:16 +0100

    added 2 droptargets

--- pokedex/src/components/BattleZone.js
+++ pokedex/src/components/BattleZone.js
@@ -4,11 +4,17 @@ import PokemonCard from "./PokemonCard";
 
 const BattleZone = () => {
   const [pokemon, setpokemon] = useState();
+  const [pokemon2, setpokemon2] = useState();
 
   const renderPokemon = (DropTarget) => {
     console.log(DropTarget.dragData);
     setpokemon(DropTarget.dragData);
   };
+  const renderPokemon2 = (DropTarget) => {
+    console.log(DropTarget.dragData);
+    setpokemon2(DropTarget.dragData);
+  };
+
   return (
     <div>
       <h1> Battle</h1>
@@ -16,9 +22,17 @@ const BattleZone = () => {
         {pokemon !== undefined ? (
           <PokemonCard pokemon={pokemon} />
         ) : (
-          <p>Drop something on me</p>
+          <div style={styles.dragDiv}><p>Drop something in me</p></div>
         )}
       </DropTarget>
+      <DropTarget targetKey="foo" onHit={renderPokemon2}>
+        {pokemon2 !== undefined ? (
+          <PokemonCard pokemon={pokemon2} />
+        ) : (
+          <div style={styles.dragDiv}><p>Drop something in me</p></div>
+        )}
+      </DropTarget>
+
     </div>
   );
 };
@@ -34,6 +48,19 @@ const styles = {
     display: "absolute",
     margin: "0",
   },
+  dragDiv: {
+    color: "#387c6d",
+    height:'200px',
+    width:'200px',
+    backgroundColor: "#443737",
+    borderRadius: "10px",
+    alignItems: "center",
+    textAllign: "center",
+    display:'flex',
+    justifyContent: 'center',
+
+  },
+
 };
 
 export default BattleZone;
